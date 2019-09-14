519c6ff2b44473631fb5196ccc1639e6d0dee4f7
Fabian Castillo <fcastill@tibco.com>
2019-09-13 17:39:27 -0700

    add mock data for exported app for stream simulator

--- apps/server/src/modules/simulator/simulatable-app-generator.ts
+++ apps/server/src/modules/simulator/simulatable-app-generator.ts
@@ -10,6 +10,108 @@ export class SimulatableAppGenerator {
       repeatInterval?: string;
     }
   ) {
-    return {};
+    const repeatInterval = options.repeatInterval || 500;
+    return getMock(options.filePath, options.port, repeatInterval);
   }
+}
+
+function getMock(filePath, port, repeatInterval) {
+  return {
+    name: '20190828-1542',
+    type: 'flogo:app',
+    version: '0.0.1',
+    description: '',
+    appModel: '1.1.0',
+    imports: [
+      'github.com/skothari-tibco/csvtimer',
+      'github.com/project-flogo/stream/activity/filter',
+      'github.com/project-flogo/contrib/activity/log',
+      'github.com/project-flogo/stream/service/telemetry',
+      'github.com/project-flogo/contrib/trigger/rest',
+      'github.com/project-flogo/stream',
+    ],
+    triggers: [
+      {
+        id: 'flogo-timer',
+        ref: 'github.com/skothari-tibco/csvtimer',
+        settings: {
+          port: `${port}`,
+          control: true,
+        },
+        handlers: [
+          {
+            settings: {
+              header: true,
+              filePath: filePath,
+              repeatInterval: `${repeatInterval}`,
+            },
+            actions: [
+              {
+                id: 'simple_agg',
+                input: {
+                  foo: '=$.data.foo',
+                  bar: '=$.data.bar',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    actions: [
+      {
+        ref: '#stream',
+        settings: {
+          pipelineURI: 'res://pipeline:simple_filter',
+        },
+        id: 'simple_agg',
+      },
+    ],
+    resources: [
+      {
+        id: 'pipeline:simple_filter',
+        data: {
+          metadata: {
+            input: [
+              {
+                name: 'foo',
+                type: 'integer',
+              },
+              {
+                name: 'bar',
+                type: 'integer',
+              },
+            ],
+            output: [
+              {
+                name: 'test',
+                type: 'any',
+              },
+            ],
+          },
+          stages: [
+            {
+              ref: '#filter',
+              settings: {
+                type: 'non-zero',
+                proceedOnlyOnEmit: true,
+              },
+              input: {
+                value: '=$.foo',
+              },
+            },
+            {
+              ref: '#log',
+              input: {
+                message: '=$.value',
+              },
+              output: {
+                'pipeline.test': "='cool'",
+              },
+            },
+          ],
+        },
+      },
+    ],
+  };
 }
