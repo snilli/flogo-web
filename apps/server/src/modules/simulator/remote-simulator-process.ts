d8fde7d98dec2c4cdb874897e21ad063e83e1905
Fabian Castillo <fcastill@tibco.com>
2019-09-18 11:44:29 -0700

    refactor: move stream runner status to core.
    so it can be shared between server and client

--- apps/server/src/modules/simulator/remote-simulator-process.ts
+++ apps/server/src/modules/simulator/remote-simulator-process.ts
@@ -1,20 +1,13 @@
 import got from 'got';
 import Websocket from 'ws';
 import { StreamRunnerProcess } from '../engine/process/stream-runner-process';
+import { StreamProcessStatus } from '@flogo-web/core';
 
 type ListenerFn = (data) => any;
 const NO_OP: ListenerFn = () => {};
 
-export enum ProcessStatus {
-  New = 'new',
-  Running = 'running',
-  Paused = 'paused',
-  Closed = 'closed',
-  Errored = 'errored',
-}
-
 export class RemoteSimulatorProcess {
-  private _status: ProcessStatus = ProcessStatus.New;
+  private _status: StreamProcessStatus = StreamProcessStatus.New;
   private dataSubscription: (data) => any = NO_OP;
   private statusSubscription: (data) => any = NO_OP;
   private subprocess: StreamRunnerProcess;
@@ -36,27 +29,31 @@ export class RemoteSimulatorProcess {
   }
 
   async restart() {
-    return this.callRestControl('restart', ProcessStatus.Running);
+    return this.callRestControl('restart', StreamProcessStatus.Running);
   }
 
   async pause() {
-    return this.callRestControl('pause', ProcessStatus.Paused);
+    return this.callRestControl('pause', StreamProcessStatus.Paused);
   }
 
   async resume() {
-    return this.callRestControl('resume', ProcessStatus.Running);
+    return this.callRestControl('resume', StreamProcessStatus.Running);
   }
 
   setup(subprocess: StreamRunnerProcess) {
     this.subprocess = subprocess;
     this.updateStatus(
-      this.subprocess.isRunning() ? ProcessStatus.Running : ProcessStatus.Closed
+      this.subprocess.isRunning()
+        ? StreamProcessStatus.Running
+        : StreamProcessStatus.Closed
     );
     let isSubprocessClosed = false;
     let retries = 0;
     this.subprocess.getCurrentChildProcess().whenClosed.then(exitCode => {
       isSubprocessClosed = true;
-      this.updateStatus(exitCode === 0 ? ProcessStatus.Closed : ProcessStatus.Errored);
+      this.updateStatus(
+        exitCode === 0 ? StreamProcessStatus.Closed : StreamProcessStatus.Errored
+      );
     });
     const wsConnect = () => {
       if (isSubprocessClosed || this.subprocess !== subprocess) {
@@ -64,17 +61,20 @@ export class RemoteSimulatorProcess {
       }
       this.ws = new Websocket(this.config.wsUrl);
       this.ws.on('message', d => {
-        console.log('got message');
         try {
           this.dataSubscription(JSON.parse(d.toString()));
-        } catch (e) {}
+        } catch (e) {
+          console.warn('Could not parse message: ', d);
+          console.warn(e);
+        }
       });
       this.ws.on('open', () => {
         console.log('opened ws connection');
       });
       this.ws.on('error', (e: any) => {
-        console.error('Could not connect to telemetry ws: ', e);
+        console.warn('Could not connect to telemetry ws: ', e);
         if (e.code === 'ECONNREFUSED' && retries++ <= 5) {
+          console.warn('Retrying...');
           setTimeout(wsConnect, 3000);
         }
       });
@@ -86,6 +86,7 @@ export class RemoteSimulatorProcess {
     if (this.subprocess && this.subprocess.isRunning()) {
       this.subprocess.stop();
     }
+    this.updateStatus(StreamProcessStatus.Closed);
 
     if (this.ws && [Websocket.OPEN, Websocket.CONNECTING].includes(this.ws.readyState)) {
       try {
@@ -107,14 +108,14 @@ export class RemoteSimulatorProcess {
     this.statusSubscription = fn;
   }
 
-  private updateStatus(newStatus: ProcessStatus) {
+  private updateStatus(newStatus: StreamProcessStatus) {
     this._status = newStatus;
     if (this.statusSubscription) {
       this.statusSubscription(this._status);
     }
   }
 
-  private callRestControl(action: string, statusIfSuccess: ProcessStatus) {
+  private callRestControl(action: string, statusIfSuccess: StreamProcessStatus) {
     this.throwIfProcessUnavailable(action);
     return got(`${this.config.restControlUrl}/${action}`).then(r => {
       this.updateStatus(statusIfSuccess);
--- libs/core/src/lib/constants/index.ts
+++ libs/core/src/lib/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './expression-prefix';
 export * from './contributions-ref-placeholder';
 export * from './contribution-type';
+export * from './stream-runner';
