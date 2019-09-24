70897f85330c7f22b6261ecc3ef3e23f7909e7c8
Fabian Castillo <fcastill@tibco.com>
2019-09-23 17:56:41 -0700

    fix(build): remove "SourceMap" warnings added by perspective webpack plugin

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,5 +1,43 @@
 const PerspectivePlugin = require('@finos/perspective-webpack-plugin');
 
-module.exports = {
-  plugins: [new PerspectivePlugin()],
+module.exports = (config, options) => {
+  config.plugins.push(
+    new PerspectivePlugin(),
+    /*  temp fix to remove source map loader added by perspective plugin which causes compilation warnings */
+    {
+      apply(compiler) {
+        compiler.hooks.afterPlugins.tap(
+          'PerspectiveConfigFixerPlugin',
+          removePerspectiveSourceMapLoader
+        );
+      },
+    }
+  );
+  return config;
 };
+
+function removePerspectiveSourceMapLoader(compiler) {
+  const rules = (((compiler || {}).options || {}).module || {}).rules || [];
+  const ruleIndex = rules.findIndex(isPerspectiveSourceMapLoaderRule);
+  if (ruleIndex > -1) {
+    rules.splice(ruleIndex, 1);
+  }
+}
+
+function isSameRegex(regex1, regex2) {
+  return (
+    regex1 instanceof RegExp &&
+    regex2 instanceof RegExp &&
+    regex1.source === regex2.source
+  );
+}
+
+const matchRegex = /\.js$/;
+function isPerspectiveSourceMapLoaderRule(rule) {
+  return (
+    rule &&
+    rule.loader === 'source-map-loader' &&
+    rule.test &&
+    isSameRegex(matchRegex, rule.test)
+  );
+}
