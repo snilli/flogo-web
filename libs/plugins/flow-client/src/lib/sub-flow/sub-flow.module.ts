e91f4a1ac15b7b20873976e3d73e73d307c8cc53
Fabian Castillo <fcastill@tibco.com>
2019-10-14 15:49:07 -0700

    refactor(flow-client): integrate new subflow modal with 'add new activity' feature

--- libs/plugins/flow-client/src/lib/sub-flow/sub-flow.module.ts
+++ libs/plugins/flow-client/src/lib/sub-flow/sub-flow.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BsModalModule } from 'ng2-bs3-modal';
+import { ModalModule } from '@flogo-web/lib-client/modal';
 
 import { CoreModule as FlowCoreModule } from '@flogo-web/lib-client/core';
+import { LanguageModule } from '@flogo-web/lib-client/language';
+import { SearchModule } from '@flogo-web/lib-client/search';
 
 import { FlowsListModule } from '../shared/flows-list';
+
 import { SubFlowComponent } from './sub-flow.component';
+import { BsModalModule } from 'ng2-bs3-modal';
 
 @NgModule({
-  imports: [CommonModule, BsModalModule, FlowCoreModule, FlowsListModule],
+  imports: [
+    BsModalModule,
+    CommonModule,
+    ModalModule,
+    FlowCoreModule,
+    FlowsListModule,
+    LanguageModule,
+    SearchModule,
+  ],
   declarations: [SubFlowComponent],
   exports: [SubFlowComponent],
 })
