69fcb2b2d60aec3ba2e308ae253b849043367507
ppaidi <ppaidi@tibco.com>
2019-12-13 18:09:22 +0530

    fix: format attributes to desired format for flow test runner

--- libs/plugins/flow-client/src/lib/core/test-runner/create-run-options-for-root.ts
+++ libs/plugins/flow-client/src/lib/core/test-runner/create-run-options-for-root.ts
@@ -3,6 +3,7 @@ import { chain, cloneDeep, get, isEmpty, isNil } from 'lodash';
 import { FlowState } from '../state';
 import { MetadataAttribute } from '../interfaces';
 import { RunOptions } from './run-orchestrator.service';
+import { Dictionary } from '@flogo-web/lib-client/core';
 
 export function createRunOptionsForRoot(flowState: FlowState) {
   const initData = getInitDataForRoot(flowState);
@@ -17,18 +18,13 @@ export function createRunOptionsForRoot(flowState: FlowState) {
   return runOptions;
 }
 
-function getInitDataForRoot(
-  flowState: FlowState
-): undefined | ((MetadataAttribute & { value: any })[]) {
+function getInitDataForRoot(flowState: FlowState): Dictionary<any> | undefined {
   const flowInput = get(flowState, 'metadata.input') as MetadataAttribute[];
   if (isEmpty(flowInput)) {
     return undefined;
   }
-  return chain(flowInput)
-    .filter((item: any) => {
-      // filter empty values
-      return !isNil(item.value);
-    })
-    .map((item: any) => cloneDeep(item))
-    .value();
+  return flowInput.reduce((accumulateInput, input) => {
+    accumulateInput[input.name] = input.value;
+    return accumulateInput;
+  }, {});
 }
