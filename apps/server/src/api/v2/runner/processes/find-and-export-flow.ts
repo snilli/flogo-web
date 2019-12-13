c2d50d14a9325bad46ae87a9d9956a25cd1a341e
ppaidi <ppaidi@tibco.com>
2019-12-13 17:52:56 +0530

    refactor: code format

--- apps/server/src/api/v2/runner/processes/find-and-export-flow.ts
+++ apps/server/src/api/v2/runner/processes/find-and-export-flow.ts
@@ -1,9 +1,9 @@
 import { Context } from 'koa';
-import { rootContainer } from "../../../../init";
-import { FlowExporter } from "../../../../modules/transfer/export/flow-exporter";
+import { rootContainer } from '../../../../init';
+import { FlowExporter } from '../../../../modules/transfer/export/flow-exporter';
 
 export const findAndExportFlow = async (context: Context, next) => {
-  const flowExporter =  rootContainer.get(FlowExporter);
+  const flowExporter = rootContainer.get(FlowExporter);
   const { data } = await flowExporter.export(context.request.body.actionId);
   context.state.flow = transformToProcess(data);
   return next();
