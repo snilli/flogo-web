95e350ce48de1b705bf4197bffc0d6cb55d765c4
Anand Pandurangi <apandura@tibco.com>
2020-03-26 17:57:40 +0530

    feat(lib-client): ability to drop tiles in another tile place

--- libs/lib-client/diagram/src/lib/diagram/diagram-row.component.ts
+++ libs/lib-client/diagram/src/lib/diagram/diagram-row.component.ts
@@ -16,6 +16,7 @@ import { actionEventFactory } from '../action-event-factory';
 import { RowIndexService, isTaskTile, isInsertTile } from '../shared';
 import { rowAnimations } from './diagram-row.animations';
 import { trackTileByFn } from '../tiles/track-tile-by';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'flogo-diagram-row',
   templateUrl: './diagram-row.component.html',
@@ -59,5 +60,26 @@ export class DiagramRowComponent implements OnChanges {
 
   onTaskAction(action: DiagramAction) {
     this.action.emit(action);
+  }
+
+  updateTile(event: CdkDragDrop<Tile[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      const newIndex =
+        event.container.data.findIndex((item: TaskTile) => item.task?.type === 'branch') +
+        event.currentIndex +
+        1;
+      const prevIndex =
+        event.previousContainer.data.findIndex(
+          (item: TaskTile) => item.task?.type === 'task'
+        ) + event.previousIndex;
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        prevIndex,
+        newIndex
+      );
+    }
   }
 }
