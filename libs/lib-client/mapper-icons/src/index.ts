a09e27db7ce53085b58e5531a6ad2a2699cef317
ppaidi <ppaidi@tibco.com>
2020-06-18 17:51:14 +0530

    style(lib-client): code format

--- libs/lib-client/mapper-icons/src/index.ts
+++ libs/lib-client/mapper-icons/src/index.ts
@@ -1,4 +1,4 @@
-export { MapperIconsModule } from './lib/mapper-icons.module'
-export { MapperIconsService } from './lib/mapper-icons.service'
-export { MapperIconsComponent } from './lib/mapper-icons.component'
-export { MapperTreeNodeType } from './lib/mapper-treenode-type.interface'
+export { MapperIconsModule } from './lib/mapper-icons.module';
+export { MapperIconsService } from './lib/mapper-icons.service';
+export { MapperIconsComponent } from './lib/mapper-icons.component';
+export { MapperTreeNodeType } from './lib/mapper-treenode-type.interface';
