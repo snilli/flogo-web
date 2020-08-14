5f3f6189255c6dbf229fe814b39273f65ae1c8a2
Prajwal Pyakurel <prazol.pp@gmail.com>
2020-08-14 11:35:59 -0700

    fixed logger type error

--- apps/server/src/api/ws/engine-log-streamer.ts
+++ apps/server/src/api/ws/engine-log-streamer.ts
@@ -2,17 +2,15 @@ import * as socketio from 'socket.io';
 import { engineLogger } from '../../common/logging';
 import EventEmitter = NodeJS.EventEmitter;
 
+const CONNECTION_QUERY_PARAMS = {
+  start: 0,
+  limit: 10,
+  until: null,
+  order: 'desc' as 'desc',
+  fields: ['level', 'timestamp', 'message'],
+};
 export class EngineLogStreamer {
   private cleanup: Function;
-  private readonly firstConnectionQuery = {
-    start: 0,
-    limit: 10,
-    from: 'now',
-    until: '-24hr',
-    order: 'desc' as 'desc',
-    fields: ['level', 'timestamp', 'message'],
-  };
-
   constructor(private channel: EventEmitter) {}
 
   init() {
@@ -32,13 +30,16 @@ export class EngineLogStreamer {
   }
 
   onNewConnection(client: socketio.Socket) {
-    engineLogger.query(this.firstConnectionQuery, (err, results) => {
-      if (err) {
-        console.log(err);
+    engineLogger.query(
+      { ...CONNECTION_QUERY_PARAMS, until: new Date() },
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        const docs = results['file'] || [];
+        client.emit('on-connecting', JSON.stringify(docs.reverse()));
       }
-      const docs = results['file'] || [];
-      client.emit('on-connecting', JSON.stringify(docs.reverse()));
-    });
+    );
   }
 
   destroy() {
